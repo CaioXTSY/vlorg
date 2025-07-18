5206635c9cc4de4314e70b3c45b767e8d0025460
Caio <125697270+CaioXTSY@users.noreply.github.com>
2025-07-18 10:15:28 -0300

    refact: remove unused PrismaService import and prisma property from AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  prisma: any;
   getHello(): string {
     return 'Hello World!';
   }
